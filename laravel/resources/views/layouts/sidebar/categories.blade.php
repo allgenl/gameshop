a627653e4e26488a7f2c45186759452399e873e3
Oleg <allgenl@mail.ru>
2023-05-10 23:47:48 +0300

    Initial commit

---
+++ laravel/resources/views/layouts/sidebar/categories.blade.php
@@ -0,0 +1,12 @@
+<div class="sidebar-item">
+    <div class="sidebar-item__title">Категории</div>
+    <div class="sidebar-item__content">
+        <ul class="sidebar-category">
+            <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Action</a></li>
+            <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">RPG</a></li>
+            <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Квесты</a></li>
+            <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Онлайн-игры</a></li>
+            <li class="sidebar-category__item"><a href="#" class="sidebar-category__item__link">Стратегии</a></li>
+        </ul>
+    </div>
+</div>
